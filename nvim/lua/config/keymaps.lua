86b59800f42e2466f7aeb5421166920ea36a50e2
Ryan Selesnik <68182750+RyanSelesnik@users.noreply.github.com>
2026-01-25 11:27:50 +0000

    Add Ctrl+L keymap to auto-correct spelling errors

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Auto-correct nearest spelling error to the left of cursor
+-- [s jumps to previous misspelled word, 1z= applies first suggestion, `]a returns to original position
+vim.keymap.set("i", "<C-l>", "<C-g>u<Esc>[s1z=`]a", { desc = "Fix nearest spelling error" })
+vim.keymap.set("n", "<C-l>", "[s1z=", { desc = "Fix nearest spelling error" })
